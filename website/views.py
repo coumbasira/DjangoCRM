9e1d7a1a349faeae260e240cce8467071d1e298b
coumbasira <coumba.dev@gmail.com>
2023-08-14 14:36:27 +0000

    django login

--- website/views.py
+++ website/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    # vérifier si vous avez un compte
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #vérifie si vous etes authentifier
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Vous avez été connecté")
+            return redirect('home')
+        else:
+            messages.success(request, "une erreur s'est produite lors de la connexion, veuillez réessayer...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    pass
